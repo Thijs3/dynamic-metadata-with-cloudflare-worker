d4530264f80e9d851df500cff4576d5f05d354e1
Thijs3 <163303403+Thijs3@users.noreply.github.com>
2024-10-09 18:38:40 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 export const config = {
-  domainSource: "https://d1757b15-7b01-4da9-80e6-7a882e779306/b8c679d9-aaf7-4bc3-81fa-b4f6dcaeaa22.weweb-preview.io", // Your WeWeb app preview link
+  domainSource: "https://d1757b15-7b01-4da9-80e6-7a882e779306.weweb-preview.io", // Your WeWeb app preview link
   patterns: [
       {
           pattern: "/artikel/[^/]+",
-          metaDataEndpoint: "https://xpbn-wusf-50q5.f2.xano.io/api:_vMbLsQj/1_artikelen/artikel_metadata"
+          metaDataEndpoint: "https://xpbn-wusf-50q5.f2.xano.io/api:_vMbLsQj/1_artikelen/metadata/{artikel_id}"
       },
       {
           pattern: "/team/profile/[^/]+",
